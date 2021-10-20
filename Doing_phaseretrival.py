3cbcd1ff67b7c96f40d5575a15fdb499c495137d
unknown <shanudengre82@gmail.com>
2021-10-20 23:37:06 +0200

    Added more reconstruction related of py files

--- Doing_phaseretrival.py
+++ Doing_phaseretrival.py
@@ -8,40 +8,11 @@ from definingfft import fft_online
 from fineupphaseretirval import fienup_phase_retrieval
 
 np.random.seed(1)
-image = cv.imread('EncryptedImage.png', cv.IMREAD_GRAYSCALE)
+image = cv.imread('EncryptedImage_fft.png', cv.IMREAD_GRAYSCALE)
 magnitudes = np.abs(np.fft.fft2(image))
-result = fienup_phase_retrieval(magnitudes, beta=0.2, 
-                           steps=200, mode='hybrid', verbose=True)
+result = fienup_phase_retrieval(magnitudes, beta=0.1, 
+                           steps=10000, mode='hybrid', verbose=True)
 
-plt.show()
-plt.subplot(121)
-plt.imshow(image, cmap='gray')
-plt.title('Image')
-plt.subplot(122)
-plt.imshow(result, cmap='gray');
-plt.title('Reconstruction')
-plt.show()
-
-np.random.seed(1)
-# image = cv.imread('EncryptedImage.png', cv.IMREAD_GRAYSCALE)
-magnitudes = np.abs(np.fft.fft2(result))
-result = fienup_phase_retrieval(magnitudes, steps=100,
-                                verbose=False)
-
-plt.show()
-plt.subplot(121)
-plt.imshow(image, cmap='gray')
-plt.title('Image')
-plt.subplot(122)
-plt.imshow(result, cmap='gray');
-plt.title('Reconstruction')
-plt.show()
-
-np.random.seed(1)
-# image = cv.imread('EncryptedImage.png', cv.IMREAD_GRAYSCALE)
-magnitudes = np.abs(np.fft.fft2(result))
-result = fienup_phase_retrieval(magnitudes, steps=100,
-                                verbose=False)
 
 plt.show()
 plt.subplot(121)
--- reading_img.py
+++ reading_img.py
@@ -6,8 +6,8 @@ from numpy.core.fromnumeric import shape
 from definingfft import fft_online
 
 # reading image
-img = cv.imread('testimg_2.png', cv.IMREAD_GRAYSCALE)
-cv.imshow('testingimage', img)
+img = cv.imread('EncryptedImage_fft.png', cv.IMREAD_GRAYSCALE)
+cv.imshow('EncryptedImg_fft', img)
 print(img.shape)
 
 # img_0 = img[:, :, 0]
@@ -22,11 +22,11 @@ print(img.shape)
 #         #     pass
 # cv.imwrite('Square_root.png', img_copy)
 
-img_square = cv.imread('EncryptedImage_center_crop_auto_512.png', cv.IMREAD_GRAYSCALE)
-cv.imshow('Square', img_square)
+# img_square = cv.imread('EncryptedImage_center_crop_auto_512.png', cv.IMREAD_GRAYSCALE)
+# cv.imshow('Square', img_square)
 
-img_square_root = cv.imread('Square_root.png', cv.IMREAD_GRAYSCALE)
-cv.imshow('Square root', img_square_root)
+# img_square_root = cv.imread('Square_root.png', cv.IMREAD_GRAYSCALE)
+# cv.imshow('Square root', img_square_root)
 
 # print(img_0.all() == img_1.all() == img_2.all())
 # print(img.shape)
@@ -73,25 +73,13 @@ cv.imshow(f'Edges_100_200', img_3)
 #     plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
 #     plt.show()
 
-fft_online(argv=['EncryptedImage.png'])
+# fft_online(argv=['EncryptedImage.png'])
 
-fft_online(argv=['testimg_3.png'])
+# fft_online(argv=['testimg_3.png'])
 
-# fourior transform original image
-f = np.fft.ifft2(img)
-fshift_inv = np.fft.ifftshift(f)
-magnitude_spectrum_withoutshift = (20*np.log(np.abs(f)))
-magnitude_spectrum = (20*np.log(np.abs(fshift_inv)))
-
-plt.subplot(121),plt.imshow(magnitude_spectrum_withoutshift, cmap = 'gray')
-plt.title('magnitude_spectrum_withoutshift'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-# plt.imshow(magnitude_spectrum, cmap='gray')
-plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-plt.show()
 
 # fourior transform
-f = np.fft.fft2((magnitude_spectrum_withoutshift))
+f = np.fft.fft2((img))
 cv.imshow('f', np.sqrt(np.abs(f)))
 fshift = np.fft.ifftshift(f)
 magnitude_spectrum_withoutshift = (20*np.log(np.abs(f)))
